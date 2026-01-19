c53864b9eed82e569182bdb786b00e666e128ab3
Roland Bewick <roland.bewick@gmail.com>
2026-01-19 15:21:12 +0700

    fix: home page animation

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,12 +2,14 @@ import natureLoader from "@/assets/animations/nature_loader.lottie.txt";
 import { Button } from "@/components/ui/button";
 import { useTimer } from "@/hooks/useTimer";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
-import { History } from "lucide-react";
+import { History, Lightbulb } from "lucide-react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export function HomePage() {
   const navigate = useNavigate();
   const { start } = useTimer();
+  const [loaded, setLoaded] = useState(false);
 
   const handleStart = () => {
     start();
@@ -22,34 +24,51 @@ export function HomePage() {
           loop
           autoplay
           speed={0.5}
+          dotLottieRefCallback={(dotLottie) => {
+            if (dotLottie) {
+              dotLottie.addEventListener("play", () => setLoaded(true));
+            }
+          }}
           //className="absolute -z-10 flex-1"
         />
       </div>
-      <header className="flex justify-between items-center p-4">
-        <h1 className="text-xl font-semibold">First Thought</h1>
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => navigate("/history")}
-        >
-          <History className="h-5 w-5" />
-        </Button>
-      </header>
+      {loaded && (
+        <header className="flex justify-between items-center p-4">
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() =>
+              alert(`First Thought is a simple "choiceless awareness" meditation app.
+
+Clear your mind and return once a thought appears.
+
+Write your thoughts down to track over time.`)
+            }
+          >
+            <Lightbulb className="h-5 w-5" />
+          </Button>
+          <h1 className="text-xl font-semibold">First Thought</h1>
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => navigate("/history")}
+          >
+            <History className="h-5 w-5" />
+          </Button>
+        </header>
+      )}
 
       <main className="flex-1 flex flex-col items-center justify-center p-4">
-        {/* <p className="text-muted-foreground text-center mb-8">
-          First Thought is a simple "choiceless awareness" meditation app.
-        </p>
-        <p className="text-muted-foreground text-center mb-8">
-          Clear your mind and return once a thought appears.
-        </p>
-        <p className="text-muted-foreground text-center mb-8">
-          Write your thoughts down to track over time.
-        </p> */}
         <div className="flex items-center justify-center">
-          <Button size="lg" className="text-lg px-8 py-6" onClick={handleStart}>
-            START
-          </Button>
+          {loaded && (
+            <Button
+              size="lg"
+              className="text-lg px-8 py-6"
+              onClick={handleStart}
+            >
+              START
+            </Button>
+          )}
         </div>
       </main>
     </div>
